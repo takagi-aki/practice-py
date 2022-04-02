0bf9068c3c5490c37d4e3a0a29ca88715fd537a7
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-02 17:15:02 +0900

    とりあえず選択ソートファイル作成

---
+++ selection_sort.py
@@ -0,0 +1,24 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""selection_sort.py: 選択ソート関数定義ファイル"""
+
+__author__ = "Akinari Takagi"
+
+from typing import Iterable
+
+
+def selection_sort(values: Iterable):
+    """選択ソート
+
+    Iterableなオブジェクト内の要素をソートし
+    結果を標準出力に表示する
+    """
+
+    print('selection sort')
+    print('未完成')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
