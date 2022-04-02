f014ec0a81ba4bcbf36bfa9eb5e1ad340d9b4765
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-02 18:18:07 +0900

    とりあえず選択ソート実装

--- selection_sort.py
+++ selection_sort.py
@@ -6,6 +6,7 @@
 __author__ = "Akinari Takagi"
 
 from typing import Iterable
+from double_linked_list import DoubleLinkedList
 
 
 def selection_sort(values: Iterable):
@@ -13,10 +14,33 @@ def selection_sort(values: Iterable):
 
     Iterableなオブジェクト内の要素をソートし
     結果を標準出力に表示する
+    非破壊
+    O(n^2)
+
+    Example:
+        >>> selection_sort([5, 6, 4, 2, 1, 3])
+        selection sort
+        1 2 3 4 5 6
+        >>> selection_sort([5, 2, 4, 6, 1, 3])
+        selection sort
+        1 2 3 4 5 6
     """
 
     print('selection sort')
-    print('未完成')
+
+    buffer = list(values)
+
+    for i in range(len(buffer)):
+        min_val = buffer[i]
+        min_index = i
+        for j in range(i, len(buffer)):
+            if(buffer[j] < min_val):
+                min_val = buffer[j]
+                min_index = j
+        buffer[min_index] = buffer[i]
+        buffer[i] = min_val
+
+    print(' '.join(map(str, buffer)))
 
 
 if __name__ == '__main__':
