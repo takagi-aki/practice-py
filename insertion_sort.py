33aeaba54dd08426e41283693caf083ddfa5b139
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-02 16:21:07 +0900

    とりあえずファイル作成

---
+++ insertion_sort.py
@@ -0,0 +1,25 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""insertion_sort.py: 挿入ソート関数定義ファイル"""
+
+__author__ = "Akinari Takagi"
+
+from typing import Iterable
+
+
+def insertion_sort(values: Iterable):
+    """挿入ソート
+
+    Iterableなオブジェクト内の要素をソートし
+    結果を標準出力に表示する
+    """
+
+    print('insertion sort')
+
+    print('未完成')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
