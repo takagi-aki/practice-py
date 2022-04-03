e895ea7c1e638e453c79ca3176538b8706137f3e
Akinari Takagi <102791616+takagi-aki@users.noreply.github.com>
2022-04-03 17:00:24 +0900

    挿入ソート2パターン実装

--- insertion_sort.py
+++ insertion_sort.py
@@ -7,28 +7,76 @@ __author__ = "Akinari Takagi"
 
 from typing import Iterable
 
-from numpy import insert
+
 from double_linked_list import DoubleLinkedList
 
 
 def insertion_sort(values: Iterable):
-    """挿入ソート
+    """標準listを使用した挿入ソート
 
     Iterableなオブジェクト内の要素をソートし
     結果を標準出力に表示する
 
-    多分O(n^2)
+    n個の入力に対してO(n^2)
+    非破壊
 
     Example:
         >>> insertion_sort([5, 2, 4, 6, 1, 3])
         insertion sort
+        5 2 4 6 1 3
+        2 5 4 6 1 3
+        2 4 5 6 1 3
+        2 4 5 6 1 3
+        1 2 4 5 6 3
         1 2 3 4 5 6
         >>> insertion_sort([1, 2, 3])
         insertion sort
         1 2 3
+        1 2 3
+        1 2 3
     """
 
     print('insertion sort')
+
+    buffer = list(values)
+
+    for i in range(len(buffer)):
+        v = buffer[i]
+        j = i - 1
+        while j >= 0 and buffer[j] > v:
+            buffer[j+1] = buffer[j]
+            j -= 1
+        buffer[j+1] = v
+
+        print(' '.join(map(str, buffer)))
+
+
+def doublelinkedlist_insertion_sort(values: Iterable):
+    """DoubleLinkedListを使用した挿入ソート
+
+    Iterableなオブジェクト内の要素をソートし
+    結果を標準出力に表示する
+
+    n個の入力に対してO(n^2)
+    非破壊
+
+    Example:
+        >>> doublelinkedlist_insertion_sort([5, 2, 4, 6, 1, 3])
+        doublelinkedlist insertion sort
+        5
+        2 5
+        2 4 5
+        2 4 5 6
+        1 2 4 5 6
+        1 2 3 4 5 6
+        >>> doublelinkedlist_insertion_sort([1, 2, 3])
+        doublelinkedlist insertion sort
+        1
+        1 2
+        1 2 3
+    """
+
+    print('doublelinkedlist insertion sort')
 
     buffer = DoubleLinkedList()
 
@@ -47,7 +95,7 @@ def insertion_sort(values: Iterable):
                 buffer.insert(it, x)
                 break
 
-    print(' '.join(map(str, buffer)))
+        print(' '.join(map(str, buffer)))
 
 
 if __name__ == '__main__':
